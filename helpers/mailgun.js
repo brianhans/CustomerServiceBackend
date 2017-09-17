a709ca5309ca4be68fcb86b88da57ab0e5f9cde2
Brian Hans <wbrianw8@gmail.com>
2017-09-17 03:24:27 -0700

    Changed mailgun libraries

--- helpers/mailgun.js
+++ helpers/mailgun.js
@@ -1,16 +1,26 @@
-var MailGun = require('mailgun-es6');
-var mailGun = new MailGun({
-	privateApi: 'key-8a569eb14f2dcaef9bbdc50c019b5665',
-	publicApi: 'pubkey-ccbda0bbab946e4265b30d58c06e5f57',
-	domainName: 'sandbox24044024808a4e40bc5d2f8e22dae53c.mailgun.org'
+var api_key = 'key-8a569eb14f2dcaef9bbdc50c019b5665';
+var domain = 'sandbox24044024808a4e40bc5d2f8e22dae53c.mailgun.org';
+var mailgun = require('mailgun-js')({
+	apiKey: api_key,
+	domain: domain
 });
 
 exports.sendMessage = function sendMessage(to, from, message) {
-	return mailGun.sendEmail({
-		to: [to],
-		from: from,
-		subject: 'Reply to you message',
-		text: message,
-		attachments: {}
-	}['sandbox24044024808a4e40bc5d2f8e22dae53c.mailgun.org']);
+	return new Promise(function(resolve, reject) {
+		var data = {
+			from: from,
+			to: to,
+			subject: 'Reply to you message',
+			text: message
+		};
+
+		return mailgun.messages().send(data, function(error, body) {
+			if (error) {
+				return reject(error);
+			}
+
+			resolve(body);
+		});
+	});
+
 }
